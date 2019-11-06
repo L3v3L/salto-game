2c6e3176ce44db699fa279202fe18742abbe1f19
Ivo Barros <myL3v3L@gmail.com>
2019-11-06 17:22:43 +0000

    extracted Monster Sprite

--- src/Monster.js
+++ src/Monster.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Sprite, SpriteCanvasHelper } from "mixel";
 import styled, { keyframes } from "styled-components";
 import { bounce } from "react-animations";
+import { MonsterSprite } from "./Sprites";
 
 const bounceAnimation = keyframes`${bounce}`;
 
@@ -15,86 +16,7 @@ export class Monster extends Component {
     this.state = {};
   }
   componentDidMount() {
-    const MASK = {
-      width: 6,
-      height: 12,
-      mirrorX: true,
-      data: [
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        1,
-        1,
-        0,
-        0,
-        0,
-        0,
-        1,
-        -1,
-        0,
-        0,
-        0,
-        1,
-        1,
-        -1,
-        0,
-        0,
-        0,
-        1,
-        1,
-        -1,
-        0,
-        0,
-        1,
-        1,
-        1,
-        -1,
-        0,
-        1,
-        1,
-        1,
-        2,
-        2,
-        0,
-        1,
-        1,
-        1,
-        2,
-        2,
-        0,
-        1,
-        1,
-        1,
-        2,
-        2,
-        0,
-        1,
-        1,
-        1,
-        1,
-        -1,
-        0,
-        0,
-        0,
-        1,
-        1,
-        1,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0
-      ]
-    };
-    let sprite = new Sprite(MASK, {
+    let sprite = new Sprite(MonsterSprite, {
       colored: true,
       colorVariations: 1,
       edgeBrightness: 0,
