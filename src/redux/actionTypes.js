670e4cbc30e2fcfafccdc38817312456a134eeea
Bruno Sabença <bruno@evesleep.co.uk>
2019-11-07 14:10:20 +0000

    Extends store functionality to manage player and card data

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,2 +1,16 @@
+//monsters
 export const ADD_MONSTER = "ADD_MONSTER";
 export const ATTACK_MONSTER = "ATTACK_MONSTER";
+
+//cards
+export const CREATE_CARD = "CREATE_CARD";
+
+//player
+export const ADD_CARD_TO_DECK = "ADD_CARD_TO_DECK";
+export const REMOVE_CARD_FROM_DECK = "REMOVE_CARD_FROM_DECK";
+
+export const ADD_CARD_TO_HAND = "ADD_CAR_TO_HAND";
+export const REMOVE_CARD_FROM_HAND = "REMOVE_CARD_FROM_HAND";
+
+export const ADD_CARD_TO_DISCARD = "ADD_CARD_TO_DISCARD";
+export const REMOVE_CARD_FROM_DISCARD = "REMOVE_CARD_FROM_DISCARD";
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -8,3 +8,16 @@ export const getMonsterById = (store, id) =>
 
 export const getMonsters = store =>
   getMonsterList(store).map(id => getMonsterById(store, id));
+
+export const getPlayerState = store => store.player;
+
+export const getCardState = store => store.cards;
+
+export const getCardList = store =>
+  getCardState(store) ? getCardState(store).allIds : [];
+
+export const getCardById = (store, id) =>
+  getCardState(store) ? { ...getCardState(store).byIds[id], id } : {};
+
+export const getCards = store =>
+  getCardList(store).map(id => getCardById(store, id));
