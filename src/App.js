f3f503f9ba6cf21a220bf897ca13214ad85e0cd5
Ivo Barros <myL3v3L@gmail.com>
2019-11-10 02:52:28 +0000

    App component loads Game component instead of Battle component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Battle from "./components/Battle";
+import Game from "./components/Game";
 
 function App() {
   return (
     <div className="salto__wrapper">
-      <Battle />
+      <Game />
     </div>
   );
 }
