90948e4e6569a424fe4ccf22e63f119c4039b90e
Jorge Teixeira <jorgemrt@protonmail.com>
2019-11-11 14:42:02 +0000

    reward screen (closes #9)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import Game from "./components/Game";
-import Map from './components/Map';
+// import Reward from './components/Reward';
 
 function App() {
   return (
     <div className="salto__wrapper">
-      {/* <Game /> */}
-      <Map />
+      <Game />
+      {/* <Reward /> */}
     </div>
   );
 }
