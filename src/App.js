fc99bac36085a5e2a0dfb534b8aef66dd52aa61e
Jorge Teixeira <jorge92@mailbox.org>
2019-11-11 11:41:07 +0000

    menu screen closes (#45)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Game from "./components/Game";
+// import Start from './components/Start';
 
 function App() {
   return (
     <div className="salto__wrapper">
       <Game />
+      {/* <Start /> */}
     </div>
   );
 }
